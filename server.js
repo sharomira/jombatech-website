563e8a3bcc12d273f06d6f73a678e945b3632769
jombatech <shalommira0@gmail.com>
2026-08-31 20:24:44 +0300

    Fix PostgreSQL query syntax and add root routing

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
   // res.render('index');
 
   // Fallback: Serves static index.html or sends basic welcome message
-  res.sendFile(path.join(__dirname, 'public', 'index.html'), (err) => {
+  res.sendFile(path.join(__dirname, 'public', 'dashboard.html'), (err) => {
     if (err) {
       res.send('Welcome to Jombatech Website! Server is live.');
     }
